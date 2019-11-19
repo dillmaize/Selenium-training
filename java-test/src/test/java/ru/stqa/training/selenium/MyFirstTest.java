05e47658fba9d60f52925274ec87b55c39075e9c
Anna Kashenok <dillmaize@mail.ru>
2019-11-19 19:28:24 +0300

    First task

--- java-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,16 +1,8 @@
 package ru.stqa.training.selenium;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
 
@@ -18,7 +10,7 @@ public class MyFirstTest {
 
         WebDriver driver = new ChromeDriver();
 
-        //open selenium2.ru
+        //open seleniumhq.org
         driver.get("https://www.seleniumhq.org/");
 
         driver.close();
