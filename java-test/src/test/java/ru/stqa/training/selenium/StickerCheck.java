cff7662e7d01461c763c9d946f6fa3b49d3d0821
Anna Kashenok <dillmaize@mail.ru>
2019-11-30 20:13:48 +0300

    Task#8

---
+++ java-test/src/test/java/ru/stqa/training/selenium/StickerCheck.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class StickerCheck {
+
+    private WebDriver driverChrome;
+
+    @Before
+    public void start() {
+        driverChrome = new ChromeDriver();
+        driverChrome.get("http://localhost:8080/litecart");
+    }
+
+    @Test
+    public void checkSticker() {
+        driverChrome.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        List<WebElement> products = driverChrome.findElements(By.cssSelector("li.product"));
+        for (WebElement p : products) {
+            Assert.assertTrue(p.findElements(By.cssSelector("div.sticker")).size() == 1);
+        }
+    }
+
+    @After
+    public void stop(){
+        driverChrome.quit();
+        driverChrome = null;
+    }
+}
